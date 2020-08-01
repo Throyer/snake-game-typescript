b8d7c362238716662bd7945eda0ed46124e5e7e6
Renato Henrique <renato.henrique_98@hotmail.com>
2020-07-31 21:49:55 -0300

    First commit

---
+++ src/Engine.ts
@@ -0,0 +1,39 @@
+export default class Engine {
+  public update: (context: CanvasRenderingContext2D) => void;
+  public canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  constructor({
+    canvasId,
+    width,
+    height,
+  }: {
+    canvasId?: string;
+    width: number;
+    height: number;
+  }) {
+    const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+    if (canvas) {
+      this.canvas = canvas;
+    } else {
+      const element = document.createElement("canvas");
+      document.body.appendChild(element)
+      this.canvas = element;
+    }
+
+    this.context = this.canvas.getContext("2d");
+    this.canvas.width = width;
+    this.canvas.height = height;
+  }
+
+  public start(): void {
+    this.loop();    
+  }
+
+  private loop(): void {
+    if (this.update) {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.update(this.context);
+    }
+    requestAnimationFrame(this.loop.bind(this));
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Engine from "./Engine";
+
+const engine = new Engine({ width: 800, height: 600 })
+
+engine.update = context => { }
+
+engine.start();
